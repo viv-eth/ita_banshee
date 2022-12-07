f1b554ae8c3cf5f2f0d3aeca22a5a76b3fb43c48
viv-eth <vivianep@ethz.ch>
2022-12-07 09:37:31 +0100

    softmax: fix implementation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,118 @@
+extern crate ndarray;
+extern crate rand;
+extern crate round;
+extern crate npy;
+
+use ndarray:: {
+    prelude::*,
+    Array2,
+};
+use npy::NpyData;
+use std::io::Read;
+
+use round::{
+    round, 
+    round_up, 
+    round_down};
+
+use rand::Rng;
+
 mod softmax;
 use softmax::
 {
-    softmax_test,
-    soft_max_s8,
+    streaming_partial_softmax,
 };
 
 fn main() {
-    softmax_test();
-    let data_in = [1,2,3,4,5];
-    let data_out: Vec<i8> = vec![0; 5]; 
-    soft_max_s8(&data_in, data_out,1,1,1,1,1,1,1);
+    let mut A_requant = Array2::<i32>::zeros((64, 64));
+    let mut Q = Array2::<i8>::zeros((64, 64));
+    let mut W_q = Array3::<i8>::zeros((1, 64, 64));
+    let mut B_q = Array3::<i8>::zeros((1, 64, 64));
+    let mut A_partial_softmax = Array2::<i32>::zeros((64, 64));
+    let mut A_temp = Array1::<i32>::zeros((64 * 64));
+    let mut Q_temp = Array1::<i8>::zeros((64 * 64));
+    let mut W_q_temp = Array1::<i8>::zeros((1 * 64 * 64));
+    let mut B_q_temp = Array1::<i8>::zeros((1 * 64 * 64));
+
+    // Init A_requant with random numbers
+    // for i in 0..64 {
+    //     for j in 0..64 {
+    //         A_requant[[i, j]] = rand::thread_rng().gen_range(-128..127);
+    //     }
+    // }
+
+    let mut A_requant_buf = vec![]; 
+    std::fs::File::open("/scratch/vivianep/ita_mempool/ita/Python_model/A_requant.npy").unwrap()
+        .read_to_end(&mut A_requant_buf).unwrap();
+    
+    let A_requant_matrix: NpyData<i64> = NpyData::from_bytes(&A_requant_buf).unwrap();
+
+    // let A_requant_matrix: NpyData<i64> = NpyData::from_bytes(&buf).unwrap();
+
+    let mut cnt = 0;
+
+    for number in A_requant_matrix {
+        A_temp[cnt] = number as i32;
+        cnt += 1;
+    }
+
+    // instantiate A_requant with data from A_temp
+    for i in 0..A_requant.shape()[0] {
+        for j in 0..A_requant.shape()[1] {
+            A_requant[[i, j]] = A_temp[i * 64 + j];
+        }
+    }
+
+    let mut Q_buf = vec![]; 
+    std::fs::File::open("/scratch/vivianep/ita_mempool/ita/Python_model/Q_matrix.npy").unwrap()
+        .read_to_end(&mut Q_buf).unwrap();
+
+    // let Q_data: NpyData<i8> = NpyData::from_bytes(&Q_buf).unwrap();
+    let Q_matrix: NpyData<i64> = NpyData::from_bytes(&Q_buf).unwrap();
+
+    // convert Q_matrix to i8
+    let mut Q_cnt = 0;
+    for number in Q_matrix {
+        Q_temp[Q_cnt] = number as i8;
+        Q_cnt += 1;
+    }
+
+    // instantiate Q with data from Q_temp
+    for i in 0..Q.shape()[0] {
+        for j in 0..Q.shape()[1] {
+            Q[[i, j]] = Q_temp[i * 64 + j]
+        }
+    }
+
+    let mut W_q_buf = vec![];
+    std::fs::File::open("/scratch/vivianep/ita_mempool/ita/Python_model/Wq_matrix.npy").unwrap()
+        .read_to_end(&mut W_q_buf).unwrap();
+
+    let W_q_matrix: NpyData<i64> = NpyData::from_bytes(&W_q_buf).unwrap();
+
+    let mut W_q_cnt = 0;
+
+    for number in W_q_matrix {
+        W_q_temp[W_q_cnt] = number as i8;
+        W_q_cnt += 1;
+    }
+
+    // instantiate W_q with data from W_q_temp
+    for i in 0..W_q.shape()[0] {
+        for j in 0..W_q.shape()[1] {
+            for k in 0..W_q.shape()[2] {
+                W_q[[i, j, k]] = W_q_temp[i * 64 * 64 + j * 64 + k];
+            }
+        }
+    }
+    
+    streaming_partial_softmax(&mut A_requant, &mut A_partial_softmax, 64);
+    // println!("A_requant: {}", A_requant);
+    // println!("A_partial_softmax: {}", A_partial_softmax);
+    println!("A_partial_softmax shape: {:?}", A_partial_softmax.shape());
+    println!("Q shape: {:?}", Q.shape());
+    println!("W_q shape: {:?}", W_q.shape());
+    println!("B_q shape: {:?}", B_q.shape());
     println!("Hello, world!");
+    // println!("cnt: {}", cnt);
 }
---
+++ src/softmax.rs
@@ -0,0 +1,93 @@
+use ndarray:: {
+    prelude::*,
+    s,
+    Array,
+    Array1,
+    Array2,
+    Array3,
+};
+
+pub fn projection_space_projection(Q: &mut Array2<i8>, W_q: &mut Array3<i8>, B_q: &mut Array3<i8>, bias: u8) {
+
+    if bias == 1 {
+        for i in 0..64 {
+            for j in 0..64 {
+                Q[[i, j]] = W_q[[0, i, j]] + B_q[[0, i, j]];
+            }
+        }
+    } else {
+        for i in 0..64 {
+            for j in 0..64 {
+                Q[[i, j]] = W_q[[0, i, j]];
+            }
+        }
+    }
+}
+
+//Compute the approximated softmax function.
+pub fn streaming_partial_softmax(A_requant: &mut Array2<i32>, A_partial_softmax: &mut Array2<i32>, seq_len: i32) {
+    
+    let log2e: f64 = f64::log2(f64::exp(1.0));
+    let x = Array::linspace(-255f32, 0.0, 256);
+    let b = 8;
+    let eps_x = b as f64 / (2.0f64.powi(b) * log2e);
+    let mut exp_partial_sum = Array1::<i32>::zeros(seq_len as usize);
+    let mut max = Array1::<i8>::zeros(64);
+    let mut current_max = Array1::<i8>::zeros(64);
+    let mut max_test: i32 = 0;
+    for i in 0..4 {
+        let A_requant_slice = A_requant.slice_mut(s![.., i * 16..(i + 1) * 16]);
+        
+        for i in 0..A_requant_slice.nrows() {
+            current_max[[i]] = A_requant_slice.row(i).iter().copied().max().unwrap() as i8;
+        }
+        
+        for j in 0..seq_len {
+            let mut shift_sum;
+            if i==0 || current_max[j as usize] > max[[j as usize]] {
+                if i==0 {
+                    shift_sum = 0;
+                } else {
+                    shift_sum = (current_max[j as usize]-max[[j as usize]])/32; 
+                    if ((((current_max[j as usize]-max[[j as usize]])/32))-shift_sum) as f64 >= 0.5 {
+                        shift_sum += 1;
+                    }
+                    max[j as usize] = current_max[j as usize];
+                }
+            } else {
+                shift_sum = 0;
+            }
+            let qb = A_requant.slice_mut(s![.., i*16..(i+1)*16]).mapv(|x| x - (max[[j as usize]]) as i32);
+            // println!("qb: {}", qb);
+            // println!("qb shape: {:?}", qb.shape());
+            let mut qexp = 0;
+            for k in 0..qb.ncols() {
+                let mut shift = (-qb[[j as usize,k]]) as i32 /32;
+                let shift_int = (-qb[[j as usize,k]]) as i32;
+                // TODO: Why is this 0.5???
+                // if ((-qb[[j as usize,k]]/32) - shift)  >= 0.5 {
+                //     shift += 1;
+                // }
+                if shift_int % 32 >= 16 {
+                    shift += 1;
+                }
+                qexp += (2.0f64.powi(10) / 2.0f64.powi(shift)) as i32;
+            }
+            exp_partial_sum[[j as usize]] = (exp_partial_sum[[j as usize]] >> shift_sum as i32) + qexp;
+        }
+    }
+    for j in 0..seq_len {
+        let factor = (((2.0f64.powi(8) - 1.0) * 2.0f64.powi(10)) as i32 / exp_partial_sum[j as usize]);
+        for k in 0..seq_len {
+            // What is going on with the types???
+            let mut shift = ((max[j as usize]-(A_requant[[j as usize,k as usize]])as i8)/32) as i32;
+            let shift_int = max[j as usize]-(A_requant[[j as usize,k as usize]])as i8;
+            if shift_int % 32 >= 16 {
+                shift += 1;
+            }
+            A_partial_softmax[[j as usize,k as usize]] = (factor as i32) / 2.0f64.powi(shift) as i32;
+        }
+    }
+
+    println!("A_partial_softmax: {}", A_partial_softmax);
+}
